cdbfa272f98e431a922b62c20129f67260817057
AiSirachcha <AiSirachcha21@.github.com>
2020-03-29 06:07:49 +0530

    Completed App(Pre-Test)

--- app/src/main/java/com/translate/literally/TranslateActivity.java
+++ app/src/main/java/com/translate/literally/TranslateActivity.java
@@ -10,29 +10,42 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.ibm.cloud.sdk.core.http.HttpMediaType;
+import com.ibm.cloud.sdk.core.service.exception.ServiceUnavailableException;
+import com.ibm.watson.developer_cloud.android.library.audio.StreamPlayer;
 import com.ibm.watson.language_translator.v3.LanguageTranslator;
 import com.ibm.watson.language_translator.v3.model.TranslateOptions;
 import com.ibm.watson.language_translator.v3.model.TranslationResult;
+import com.ibm.watson.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.text_to_speech.v1.model.GetVoiceOptions;
+import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions;
+import com.ibm.watson.text_to_speech.v1.model.Voice;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -44,15 +57,31 @@ public class TranslateActivity extends AppCompatActivity {
 	private static final String VER = "2018-05-01";
 	public static final String EXTRA_TEXT = "com.translate.literally.EXTRA_TEXT";
 	private static final int EXTRA_SUBTITLE_REQUEST = 1;
+	private AlphaAnimation inAlphaAnimation, outAlphaAnimation; //Alpha Animation for TTS load time
+	private RelativeLayout sourceTextHolder;
+	private CardView resultTextHolder;
 	private ProgressBar progressBar;
-	private ImageView placeholderIcon;
+	private ImageButton ttsSource, ttsResult;
+	private ImageView placeholderIcon;	//Translation Icon
 	private Spinner sourceSpinner, targetSpinner;
 	private EditText sourceEditText, translationResultEditText;
 	private MaterialButton savedPhraseBtn;
 	private List<Language> languages;
 	private LanguageViewModel languageViewModel;
-	private TextTranslator textTranslator = new TextTranslator(new TranslatorService().initService(API_KEY,URL,VER ));
-	RotateAnimation rotateAnimation = new RotateAnimation(0, 720, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+	private TextToSpeech textToSpeech;
+	private StreamPlayer player = new StreamPlayer(); //Stream Player required for TTS
+	private TextTranslator textTranslator = new TextTranslator(new TranslatorService().initService(API_KEY,URL,VER));
+
+	private FrameLayout ttsProgressBarHolder;
+
+	//Rotation Animation required for translation icon animation
+	private RotateAnimation rotateAnimation = new RotateAnimation(
+			0,
+			720,
+			Animation.RELATIVE_TO_SELF,
+			0.5f,
+			Animation.RELATIVE_TO_SELF,
+			0.5f);
 
 
 	@Override
@@ -70,6 +99,8 @@ public class TranslateActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_translate);
 
+		sourceTextHolder =findViewById(R.id.sourceEditTextHolder);
+		resultTextHolder = findViewById(R.id.translationResultTextViewHolder);
 		sourceSpinner = findViewById(R.id.sourceLangSpinner);
 		targetSpinner = findViewById(R.id.targetLangSpinner);
 		sourceEditText = findViewById(R.id.sourceEditText);
@@ -78,19 +109,16 @@ public class TranslateActivity extends AppCompatActivity {
 		progressBar = findViewById(R.id.progress_horizontal);
 		placeholderIcon = findViewById(R.id.placeholderIcon);
 		savedPhraseBtn= findViewById(R.id.savedPhraseBtn);
+		ttsSource = findViewById(R.id.sourceTextSpeakBtn);
+		ttsResult = findViewById(R.id.resultTextSpeakBtn);
+		ttsProgressBarHolder = findViewById(R.id.progressBarHolder);
+
+		showSnackbar(placeholderIcon, "You can click on the translate icon in order to translate");
 
 		rotateAnimation.setDuration(1500);
 		rotateAnimation.setRepeatCount(2);
 
-		Snackbar.make(placeholderIcon,
-				"You can click on the translate icon in order to translate",
-				5000)
-				.setBackgroundTint(getColor(R.color.colorSecondary))
-				.setTextColor(getColor(R.color.colorPrimary))
-				.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE)
-				.setAction("Ok", View::cancelPendingInputEvents)
-				.setActionTextColor(getColor(R.color.colorPrimary))
-				.show();
+
 
 
 		new getDataAsyncTask(TranslateActivity.this).execute();
@@ -100,18 +128,42 @@ public class TranslateActivity extends AppCompatActivity {
 			startActivityForResult(intent, EXTRA_SUBTITLE_REQUEST);
 		});
 
-		placeholderIcon.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				new TranslateTextTask(TranslateActivity.this, textTranslator.getLanguageTranslator())
-						.execute(sourceEditText.getText().toString());
-				placeholderIcon.startAnimation(rotateAnimation);
-			}
+		placeholderIcon.setOnClickListener(view -> {
+			new TranslateTextTask(TranslateActivity.this, textTranslator.getLanguageTranslator())
+					.execute(sourceEditText.getText().toString());
+			placeholderIcon.startAnimation(rotateAnimation);
 		});
 	}
 
+	public void speak(View view){
+		Log.i("speakMethod", "Entered");
 
+		textToSpeech = new TranslatorService().initService(
+						getString(R.string.TEXT_VOICE_API_KEY),
+						getString(R.string.TEXT_VOICE_URL));
+
+		if (view.getTag().toString().equalsIgnoreCase("source")){
+			new SynthesisTask(TranslateActivity.this).execute(
+					sourceEditText.getText().toString()
+			);
+		}else{
+			new SynthesisTask(TranslateActivity.this).execute(
+					translationResultEditText.getText().toString()
+			);
+		}
+	}
+
+	public void showSnackbar(View view,String message){
+		Snackbar.make(view,
+				message,
+				5000)
+				.setBackgroundTint(getColor(R.color.colorSecondary))
+				.setTextColor(getColor(R.color.colorPrimary))
+				.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE)
+				.setAction("Ok", View::cancelPendingInputEvents)
+				.setActionTextColor(getColor(R.color.colorPrimary))
+				.show();
+	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -126,9 +178,10 @@ public class TranslateActivity extends AppCompatActivity {
 
 	}
 
+	/**
+	 * This method is for a keyboard event to trigger the translation.
+	 */
 	private TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
-
-
 		@Override
 		public boolean onEditorAction(TextView textView, int action, KeyEvent keyEvent) {
 			if (action == EditorInfo.IME_ACTION_DONE) {
@@ -148,6 +201,101 @@ public class TranslateActivity extends AppCompatActivity {
 		}
 	};
 
+	/**
+	 * IBM's TTS Activation
+	 */
+	private static class SynthesisTask extends AsyncTask<String, Void, String> {
+		private WeakReference<TranslateActivity> translateActivityWeakReference;
+
+		SynthesisTask(TranslateActivity translateActivity) {
+			this.translateActivityWeakReference = new WeakReference<>(translateActivity);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			TranslateActivity activity = translateActivityWeakReference.get();
+			super.onPreExecute();
+			activity.inAlphaAnimation = new AlphaAnimation(0f, 1f);
+			activity.inAlphaAnimation.setDuration(200);
+			activity.ttsProgressBarHolder.setAnimation(activity.inAlphaAnimation);
+			activity.ttsProgressBarHolder.setVisibility(View.VISIBLE);
+			activity.sourceEditText.setEnabled(false);
+			activity.sourceTextHolder.setVisibility(View.INVISIBLE);
+			activity.resultTextHolder.setVisibility(View.INVISIBLE);
+			activity.placeholderIcon.setEnabled(false);
+			activity.savedPhraseBtn.setEnabled(false);
+			activity.ttsResult.setEnabled(false);
+			activity.ttsSource.setEnabled(false);
+
+			new CountDownTimer(3000, 1000){
+				@Override
+				public void onTick(long l) {
+
+				}
+
+				@Override
+				public void onFinish() {
+					activity.inAlphaAnimation = new AlphaAnimation(1f, 0f);
+					activity.inAlphaAnimation.setDuration(200);
+					activity.ttsProgressBarHolder.setAnimation(activity.inAlphaAnimation);
+					activity.ttsProgressBarHolder.setVisibility(View.GONE);
+					activity.sourceEditText.setEnabled(true);
+					activity.sourceTextHolder.setVisibility(View.VISIBLE);
+					activity.resultTextHolder.setVisibility(View.VISIBLE);
+					activity.placeholderIcon.setEnabled(true);
+					activity.savedPhraseBtn.setEnabled(true);
+					activity.ttsResult.setEnabled(true);
+					activity.ttsSource.setEnabled(true);
+				}
+			}.start();
+		}
+
+		@Override
+		protected String doInBackground(String... strings) {
+			TranslateActivity translateActivity = translateActivityWeakReference.get();
+
+			//https://cloud.ibm.com/apidocs/text-to-speech/text-to-speech?code=java#get-a-voice
+			Voice voice = TranslatorService.identifyLang(
+					translateActivity.textToSpeech,
+					translateActivity.textTranslator.getLanguageTranslator(),
+					strings[0]);
+
+			//https://cloud.ibm.com/apidocs/text-to-speech/text-to-speech?code=java#get-a-voice
+			SynthesizeOptions synthesizeOption= new SynthesizeOptions.Builder()
+					.text(strings[0])
+					.voice(voice.getName())
+					.accept(HttpMediaType.AUDIO_WAV)
+					.build();
+
+			try{
+				translateActivity.player.playStream(translateActivity.textToSpeech.synthesize(synthesizeOption)
+						.execute()
+						.getResult()
+				);
+			}catch (ServiceUnavailableException serviceException){
+				return "Service Exception";
+			}
+			return "Did Synthesize";
+		}
+
+		@Override
+		protected void onPostExecute(String s) {
+			TranslateActivity activity = translateActivityWeakReference.get();
+			if (s.equalsIgnoreCase("Service Exception")){
+				activity.showSnackbar(
+						activity.translationResultEditText,
+						"There was an issue with the IBM Watson Service. Please bare with us until it is solved.");
+			}
+
+		}
+
+
+	}
+
+
+	/**
+	 * This Async Task is used to populate the spinners in the UI
+	 */
 	public static class getDataAsyncTask extends AsyncTask<Void, Void, List<Language>> {
 		private WeakReference<TranslateActivity> translateActivityWeakReference;
 
@@ -183,6 +331,10 @@ public class TranslateActivity extends AppCompatActivity {
 		}
 	}
 
+
+	/**
+	 * Async Task used to translate text
+	 */
 	private static class TranslateTextTask extends AsyncTask<String, Void, String> {
 
 		private WeakReference<TranslateActivity> translateActivityWeakReference;
--- app/src/main/java/com/translate/literally/TranslatorService.java
+++ app/src/main/java/com/translate/literally/TranslatorService.java
@@ -5,14 +5,13 @@ import android.util.Log;
 import com.ibm.cloud.sdk.core.security.Authenticator;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.language_translator.v3.LanguageTranslator;
-import com.ibm.watson.language_translator.v3.model.IdentifiableLanguage;
-
-import java.util.List;
+import com.ibm.watson.language_translator.v3.model.IdentifiedLanguages;
+import com.ibm.watson.language_translator.v3.model.IdentifyOptions;
+import com.ibm.watson.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.text_to_speech.v1.model.GetVoiceOptions;
+import com.ibm.watson.text_to_speech.v1.model.Voice;
 
 class TranslatorService {
-
-
-
 	LanguageTranslator initService(String API_KEY, String URL, String VERSION_DATE){
 		Authenticator authenticator = new IamAuthenticator(API_KEY);
 		LanguageTranslator service = new LanguageTranslator(VERSION_DATE, authenticator);
@@ -20,6 +19,69 @@ class TranslatorService {
 
 		Log.i("API_CRED", API_KEY + " : "+URL+" : "+VERSION_DATE );
 		return service;
+	}
+
+	TextToSpeech initService(String API_KEY, String URL){
+		IamAuthenticator authenticator = new IamAuthenticator(API_KEY);
+		TextToSpeech textToSpeech = new TextToSpeech(authenticator);
+		textToSpeech.setServiceUrl(URL);
+
+		Log.i("API_CRED", "TTS/"+API_KEY + " : "+URL);
+		return textToSpeech;
+	}
+
+	static Voice identifyLang(TextToSpeech textToSpeech, LanguageTranslator translator, String text){
+		IdentifyOptions identifyOptions = new IdentifyOptions.Builder()
+				.text(text)
+				.build();
+		IdentifiedLanguages languages = translator.identify(identifyOptions)
+				.execute().getResult();
+
+		GetVoiceOptions getVoiceOptions = new GetVoiceOptions.Builder()
+				.voice(getVoiceModel(languages.getLanguages().get(0).getLanguage()))
+				.build();
+
+		Voice voice = textToSpeech.getVoice(getVoiceOptions).execute().getResult();
+
+		return voice;
+	}
+
+
+	private static String getVoiceModel(String langCode){
+		String voiceOption;
+
+		switch (langCode){
+			case "en":
+			default:
+				voiceOption = GetVoiceOptions.Voice.EN_GB_KATEV3VOICE;
+				break;
+
+			case "de":
+				voiceOption =  GetVoiceOptions.Voice.DE_DE_DIETERV3VOICE;
+				break;
+
+			case "es":
+				voiceOption =  GetVoiceOptions.Voice.ES_LA_SOFIAV3VOICE;
+				break;
+
+			case "fr":
+				voiceOption =  GetVoiceOptions.Voice.FR_FR_RENEEV3VOICE;
+				break;
+
+			case "it":
+				voiceOption =  GetVoiceOptions.Voice.IT_IT_FRANCESCAV3VOICE;
+				break;
+
+			case "ja":
+				voiceOption =  GetVoiceOptions.Voice.JA_JP_EMIV3VOICE;
+				break;
+
+			case "pt":
+				voiceOption =  GetVoiceOptions.Voice.PT_BR_ISABELAV3VOICE;
+				break;
+		}
+
+		return voiceOption;
 	}
 
 }
